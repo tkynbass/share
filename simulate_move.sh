b664a17c0d3144e37afa4d96bb0a8ff49fc17f34
tkym <m183287@hiroshima-u.ac.jp>
2018-06-06 20:21:24 +0900

    move sh

---
+++ simulate_move.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+#  simulate_move.sh
+#  
+#
+#  Created by tkym on 2018/06/06.
+#  
+
+
+##### define option (i:insertion) #####
+while getops "i:" option
+do
+case ${option} in
+i)
+result_name="$OPTARG"
+;;
+\?)
+printf "\n     Usage : simulate_move.sh [-i result_name]   \n\n "  1>&2
+exit 1
+;;
+esac
+done
+
+###### if there's not file to input #####
+if [ ! $result_name ]
+then
+printf "\n     Usage : simulate_move.sh [-i result_name]   \n\n "  1>&2
+exit 1
+fi
+
+if [ -e result/${result_name} ]
+then
+printf "\n      result/${result_name} already exists. Renew it? (\"yes\" or \"no\") : "
+read answer
+
+if [ $answer != yes ]
+then
+printf "\n\n    Please input not existence result_name.     \n\n"
+exit 0
+else
+rm -rf result/${result_name}
+fi
+fi
+
+###### result create #####
+mkdir result/${result_name}
+mkdir result/${result_name}/coordinates
+cp src/common.h result/${result_name}/
+cp src/function.h result/${result_name}/
+
+echo ""
+
+icc -O3 -xHost -DHAVE_SSE2=1 -DSFMT_MEXP=19937 -g -o $1  $1.c dSFMT/dSFMT.c -qopenmp -fast
+
+./src/simulate_move ${result_name}
