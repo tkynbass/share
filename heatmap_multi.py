65239068301a389c0571dea880cea9a3df71acc7
tkym <m183287@hiroshima-u.ac.jp>
2019-12-09 15:23:10 +0900

    multi.py

---
+++ heatmap_multi.py
@@ -0,0 +1,118 @@
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from subprocess import getoutput
+import itertools as itr
+import random
+import multiprocessing as mp
+
+def Plot_hist2d (array, filename, color):
+    
+    fig, ax1 = plt.subplots ()
+    
+    plt.xlabel ('N-S Distance (um)', fontsize=20)
+    plt.ylabel ('Distance between pos and x-axis', fontsize=20)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=10)
+    
+    ax1.hist2d (array[0], array[1], alpha=0.7, cmap=color, bins=30)
+    
+    plt.savefig (filename, transparent=True, bbox_inches='tight')
+    
+    plt.close()
+
+def subprocess (idx, send_rev, df, high_loop_list, low_loop_list, control_loop_list):
+
+    sub_high_pos = []
+    sub_low_pos = []
+    sub_chr_pos = [ [] for chr in range (3) ]
+    
+    pos_list = []
+    
+    LENGTH= 0.0966
+    
+    for index, row in df.iterrows ():
+        
+        NG = row['X':'Z'] - nuc_pos
+        NG_norm = np.linalg.norm (NG)
+        theta = np.arccos (np.dot (NS, NG) / ( np.linalg.norm (NS) * NG_norm ) )
+        pos_list.append  ([NG_norm * np.cos(theta), NG_norm * np.sin (theta)])
+    
+    df = pd.concat ( [df, pd.DataFrame (pos_list, columns=['Proj_X', 'Proj_Y'])], axis=1)
+
+    sub_high_pos.extend (df.iloc [high_part, 4:6].values * LENGTH)
+    sub_low_pos.extend (df.iloc [low_part, 4:6].values * LENGTH)
+    
+    for chr_no in range (3):
+        sub_chr_pos [chr_no].extend (df.loc [df.loc[:, 'chr']==chr_no, 'Proj_X':'Proj_Y'].values * LENGTH)
+
+    send_rev.send ([sub_high_pos, sub_low_pos, sub_chr_pos])
+
+#### main ####
+
+def main ():
+    
+    low_gene = pd.read_csv ('/Volumes/HDCL-U/tkym/support_text/noma_transcription/low_gene_5k_center.txt', sep='\s+')
+    high_gene = pd.read_csv ('/Volumes/HDCL-U/tkym/support_text/noma_transcription/high_gene_5k_center.txt', sep='\s+')
+
+    high_part = high_gene.loc [:, 'Center'].values
+    low_part = low_gene.loc [:, 'Center'].values
+
+    dir = '../chromo_exp'
+
+    file_list = getoutput ('ls 0/sample_*.txt').split()
+    df_list = [ pd.read_csv (file, header=None, usecols=[1,3,4,5], sep='\s+') for file in file_list ]
+    df.columns = ['chr', 'X', 'Y', 'Z']
+    
+    stable_df = pd.read_csv (f'{dir}/stable_status.txt', index_col=0, sep='\s+')
+    nuc_pos = stable_df.iloc[0, 0:3]
+
+    stable_spb = pd.read_csv (f'{dir}/stable_spb.txt', index_col=0, sep='\s+')
+    spb_pos = stable_spb.iloc[0, 0:3]
+
+    NS = spb_pos - nuc_pos
+
+    high_pos = []
+    low_pos = []
+    chr_pos = [ [] for chr in range (3) ]
+
+    process_list = []
+    pipe_list = []
+
+    for idx in range (len (file_list)):
+    
+        get_rev, send_rev = mp.Pipe (False)
+        p = mp.Process (target=subprocess, args=(idx, send_rev, df_list[idx], high_loop_list, low_loop_list, control_loop_list) )
+        process_list.append (p)
+        pipe_list.append (get_rev)
+        p.start ()
+    
+    for proc in process_list:
+        proc.join ()
+    
+    for x in pipe_list:
+        
+        high_pos.extend (x.recv()[0])
+        low_pos.extend (x.recv()[1])
+
+        for chr_no in range (3):
+            chr_pos [chr_no].extend (x.recv [2][chr_no])
+
+    cmap_list = ['Reds', 'Blues', 'Greens']
+
+    high_array = np.array (high_pos).T.reshape (2, len (high_pos))
+    low_array = np.array (low_pos).T.reshape (2, len (low_pos))
+
+    chr_arrays = [ np.array (pos).T.reshape (2, len (pos)) for pos in chr_pos ]
+
+    Plot_hist2d (high_array, f'{dir}/hg_map.png', 'Reds')
+    Plot_hist2d (low_array, f'{dir}/lg_map.png', 'Reds')
+
+    for chr_no in range (3):
+        Plot_hist2d (chr_arrays [chr_no], f'{dir}/chr{chr_no + 1}_map.png', cmap_list[chr_no])
+
+    return 0
+
+if __name__ == "__main__" :
+    main()
---
+++ tppv_multi.py
@@ -0,0 +1,122 @@
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from subprocess import getoutput
+import itertools as itr
+import random
+import multiprocessing as mp
+
+def Plot_ppv (x, high, low, random):
+    
+    fig, ax1 = plt.subplots ()
+    
+    plt.xlabel ('Distance (um)', fontsize=20)
+    plt.ylabel ('Frequency', fontsize=20)
+    plt.xticks(fontsize=15)
+    #     plt.yticks(fontsize=10)
+    
+    ax1.set_yticklabels([]) #y軸目盛りの削除
+    
+    a=1.0
+    
+    ax1.plot (x, random, color='black')
+    ax1.fill_between (x, random, 0, alpha=a, color='skyblue' )
+    
+    ax1.plot (x, low, color='black')
+    ax1.fill_between (x, low, 0, alpha=a, color='darkblue' )
+    
+    ax1.plot (x, high, color='black')
+    ax1.fill_between (x, high, 0, alpha=a, color='orange' )
+    
+    #     plt.savefig ('phy_dist.png', transparent=True)
+    
+#    plt.show()
+    plt.savefig ('tppv.png', transparent=True, bbox_inches="tight")
+
+    plt.close()
+
+def Plot_hist (high, low, random):
+    
+    fig, ax1 = plt.subplots ()
+    
+    BINS = 50
+    weights = np.ones_like (high) / float (len (high))
+    
+    h_array = ax1.hist (high, bins=BINS, weights=weights)
+    l_array = ax1.hist (low, bins=BINS, weights=weights)
+    r_array = ax1.hist (random, bins=BINS, weights=weights)
+    
+    plt.close()
+    
+    return h_array[1], h_array[0], l_array[0], r_array[0]
+
+def ppv_func (x):
+
+    return - np.log ( (x - 0.1713) / 1.0965) / 0.6865
+
+def subprocess (idx, send_rev, df, high_loop_list, low_loop_list, control_loop_list):
+
+
+    high_pd = pd.Series ([ np.linalg.norm (df.iloc [X, :] - df.iloc [Y, :] ) * 0.0966 for X, Y in high_loop_list ])
+    low_pd = pd.Series ([ np.linalg.norm (df.iloc [X, :] - df.iloc [Y, :] ) * 0.0966 for X, Y in low_loop_list ])
+    control_pd =  pd.Series ([ np.linalg.norm (df.iloc [X, :] - df.iloc [Y, :] ) * 0.0966 for X, Y in control_loop_list ])
+    
+    high_tppv = sum (ppv_func ( high_pd [ (high_pd > 0.1713) & (high_pd < 1.2678 )]) )
+    low_tppv = sum (ppv_func ( low_pd [ (low_pd > 0.1713) & (low_pd < 1.2678 )]) )
+    control_tppv = sum (ppv_func ( control_pd [ (control_pd > 0.1713) & (control_pd < 1.2678 )]) )
+
+    send_rev.send ([high_tppv, low_tppv, control_tppv])
+
+#### main ####
+
+def main ():
+    
+    low_gene = pd.read_csv ('low_gene_5k_center.txt', sep='\s+')
+    high_gene = pd.read_csv ('high_gene_5k_center.txt', sep='\s+')
+
+    high_part = high_gene.loc [:, 'Center'].values
+    low_part = low_gene.loc [:, 'Center'].values
+
+    file_list = getoutput ('ls 0/sample_*.txt').split()
+    df_list = [ pd.read_csv (file, header=None, usecols=[3,4,5], sep='\s+') for file in file_list ]
+
+    high_rand = random.sample (set (high_part), k=80)
+    low_rand = random.sample (set (low_part), k=80)
+    control_rand = random.sample ( range (2516), k=80)
+
+    high_loop_list = [ (x, y) for x, y in itr.combinations (high_rand, 2)]
+    low_loop_list = [ (x, y) for x, y in itr.combinations (low_rand, 2)]
+    control_loop_list = [ (x, y) for x, y in itr.combinations (control_rand, 2) ]
+
+    high_tppv = []
+    low_tppv = []
+    control_tppv = []
+
+    process_list = []
+    pipe_list = []
+
+    for idx in range (len (file_list)):
+    
+        get_rev, send_rev = mp.Pipe (False)
+        p = mp.Process (target=subprocess, args=(idx, send_rev, df_list[idx], high_loop_list, low_loop_list, control_loop_list) )
+        process_list.append (p)
+        pipe_list.append (get_rev)
+        p.start ()
+    
+    for proc in process_list:
+        proc.join ()
+    
+    result_list = [ x.recv() for x in pipe_list ]
+    tppv_array = np.array (result_list).reshape (3, len (result_list))
+
+    bin_class,  h_array, l_array, r_array = Plot_hist (tppv_array[0], tppv_array[1], tppv_array[2])
+    delta = bin_class[1] - bin_class[0]
+    x = [ bin_class[num] + delta  for num in range (len (bin_class) -1) ]
+
+    Plot_ppv (x, h_array, l_array, r_array)
+
+    return 0
+
+if __name__ == "__main__" :
+    main()
